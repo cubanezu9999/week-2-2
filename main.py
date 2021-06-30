4caa120f12052875734e824557d1e856f6920092
cubanezu9999 <71436676+cubanezu9999@users.noreply.github.com>
2021-06-30 13:46:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+grade = int(input('Please enter your grade: '))
+
+if grade >= 90 : print('A1')
+elif (grade >=80 and grade < 90): print('A2')
+elif (grade >=70 and grade < 80): print('A3')
+elif (grade >=65 and grade < 70): print('B1')
+elif (grade >=60 and grade < 65): print('B2')
+elif (grade >=60 and grade < 65): print('B3')
+elif (grade >=55 and grade < 60): print('C1')
+elif (grade >=50 and grade < 55): print('C2')
+elif (grade >=45 and grade < 50): print('C3')
+elif (grade >=40 and grade < 45): print('D')
+
+else : print('Fail')
